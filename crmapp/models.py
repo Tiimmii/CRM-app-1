a53924f46063b4e04cef83188da7fca45b7357ad
Tiimmii <104578848+Tiimmii@users.noreply.github.com>
2022-07-07 18:27:59 +0100

    models

--- crmapp/models.py
+++ crmapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Lead(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    first_name=models.CharField(max_length=20, null=True, blank=True)
+    last_name=models.CharField(max_length=20, null=True, blank=True)
+    age=models.IntegerField(default=0)
+    
