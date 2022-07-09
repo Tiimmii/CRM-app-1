fada6ceeb07a95674b6a0cadbdc1159d7807822e
Tiimmii <104578848+Tiimmii@users.noreply.github.com>
2022-07-09 19:52:36 +0100

    commiy

--- crmapp/forms.py
+++ crmapp/forms.py
@@ -4,10 +4,4 @@ from . models import Lead
 class LeadCreateForm(forms.ModelForm):
     class Meta:
         model = Lead
-        fields = (
-            "first_name",
-            "last_name",
-            "age",
-            "agent",
-            "image",
-        )
+        fields = '__all__'
--- crmapp/urls.py
+++ crmapp/urls.py
@@ -5,5 +5,6 @@ app_name="leads"
 
 urlpatterns = [
     path('leads-view/',views.view,name='view'),
-    path('leads-details/<str:pk>/', views.details, name='details')
+    path('leads-details/<str:pk>/', views.details, name='details'),
+    path('leads-create/', views.create, name='create'),
 ]
