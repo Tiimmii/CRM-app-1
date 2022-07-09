0f07d9e763e08bff1d4ba17429a80981a5fa5939
Tiimmii <104578848+Tiimmii@users.noreply.github.com>
2022-07-09 20:59:30 +0100

    improving the views.py

--- crmapp/views.py
+++ crmapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import Lead, Agent, User
 from . forms import LeadCreateForm
+from django.contrib import messages
+
 def view(request):
     lead = Lead.objects.all()
     return render(request, 'leads-view.html', {'lead':lead})
@@ -8,3 +10,18 @@ def view(request):
 def details(request, pk):
     lead = Lead.objects.get(id=pk)
     return render(request, 'leads-details.html', {'lead':lead})
+
+def create(request):
+    form = LeadCreateForm()
+    
+    if request.method == 'POST':
+        form=LeadCreateForm(request.POST)
+        if form.is_valid():
+            form.save()    
+            return redirect('leads:view')
+        else:
+            messages.info('unsuccessful :(')
+            return redirect('leads:create')
+    
+    else:
+        return render(request, 'leads-create.html', {'form':form})
